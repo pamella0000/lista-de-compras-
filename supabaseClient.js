51bc8342d1da5c4dd700cc74f15a70d588dea8f6
pamella0000 <ellapam505@gmail.com>
2026-03-05 10:38:35 -0300

    hunhu

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://ixugmhmmnvobcnzkqygu.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4dWdtaG1tbnZvYmNuemtxeWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE1NzIsImV4cCI6MjA4NzY4NzU3Mn0.jRmwGRlHik1oActu6K07qpLRaLzFL0JNlnoRyYhAtDQ'
+  'https://llzdvsauefvavbrgnswv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsemR2c2F1ZWZ2YXZicmduc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MDg2NTEsImV4cCI6MjA4ODI4NDY1MX0.nP1qToZN057uNBs3wQKhKMhrezSmz-FgU5SkMXpFAdg'
 )
-    
+  
